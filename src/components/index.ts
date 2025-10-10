a5c47f195f82d6b1c6b9cea0b6ecb3e46be6705d
Soofmax <salwaessaf@gmail.com>
2025-10-10 22:15:11 +0000

    feat: add index exports for components and pages
    Co-authored-by: Genie <genie@cosine.sh>

---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export { default as GoogleReviewsBadge } from './GoogleReviewsBadge';
+export { default as Reviews } from './Reviews';
---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as CityPage } from './CityPage';
+export { default as ServicesPage } from './ServicesPage';
+export { default as ContactPage } from './ContactPage';
+export { default as ZonesIndex } from './ZonesIndex';
